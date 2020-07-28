59104a844882fdae1c7edc383e274df615bf95c8
GuilleLink <32148569+GuilleLink@users.noreply.github.com>
2020-07-28 16:01:13 -0600

    Init del proyecto con Webpack, Babel y React

---
+++ juego1/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => {
+  return <div>Hello to game2</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
